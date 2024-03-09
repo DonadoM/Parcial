bdf358afefefa63cbae12b8e22f7309a5e2a765f
DonadoM <156842239+DonadoM@users.noreply.github.com>
2024-03-08 23:34:59 -0500

    Update PaymentMethods.py

--- PaymentMethods.py
+++ PaymentMethods.py
@@ -1,20 +1,23 @@
+# Importa la clase Enum de la biblioteca enum y las clases FastAPI y HTTPException de FastAPI
 from enum import Enum
 from fastapi import FastAPI, HTTPException
 
+# Crea una instancia de la aplicación FastAPI
 app = FastAPI()
 
-
+# Define una enumeración llamada PaymentMethodsTypes que hereda de str y Enum
 class PaymentMethodsTypes(str, Enum):
     CREDIT = "CREDIT"
     DEBIT = "DEBIT"
     TRANSFER = "TRANSFER"
     CASH = "CASH"
 
-
+# Define un endpoint para procesar pagos utilizando el método HTTP POST
+# El parámetro path payment_method es validado con la enumeración PaymentMethodsTypes
 @app.post("/process_payment/{payment_method}")
 async def process_payment(payment_method: PaymentMethodsTypes):
+    # Verifica el tipo de método de pago y responde en consecuencia
     if payment_method == PaymentMethodsTypes.CREDIT:
-
         return {"message": "Pago con tarjeta de crédito procesado exitosamente"}
     elif payment_method == PaymentMethodsTypes.DEBIT:
         return {"message": "Pago con tarjeta de débito procesado exitosamente"}
@@ -23,4 +26,5 @@ async def process_payment(payment_method: PaymentMethodsTypes):
     elif payment_method == PaymentMethodsTypes.CASH:
         return {"message": "Pago en efectivo procesado exitosamente"}
     else:
+        # Si el método de pago no es válido, devuelve un error HTTP 400
         raise HTTPException(status_code=400, detail="Método de pago no válido")
